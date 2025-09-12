35040ee9337cd562a89942254d9501fd285c7c5c
FACULTADES\alumnoFI <asdsad@gmail.com>
2025-09-11 22:15:35 -0300

    nuevos ejercicios matrices

--- Prácticos y ejercicios/Practico4 matrices/src/matrices/Matrices.java
+++ Prácticos y ejercicios/Practico4 matrices/src/matrices/Matrices.java
@@ -13,7 +13,12 @@ public class Matrices {
         //System.out.println("Máximo: " + maximoMatriz(mat));
         //mostrarColumna(mat, 1);
         //mostrarFila(mat, 2);
-        mostrarFilasImpares(mat);
+        //mostrarFilasImpares(mat);
+        //System.out.println("Está elemento en matriz: " + buscarElementoEnMatriz(mat, 11));
+        //mostrarSumaCol(mat);
+        //System.out.println("Tiene 3 consecutivos: " + tieneTresConsecutivosEnFila(mat));
+        //diagonalInversa(mat);
+        diagonalInversaRec(mat, 2);
     }
 
     public static void mostrarMatriz(int[][] mat) {
@@ -85,4 +90,65 @@ public class Matrices {
         }
     }
 
+    public static boolean buscarElementoEnMatriz(int[][] mat, int elemento){
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if(mat[i][j] == elemento) return true;
+            }
+        }
+        return false;
+    }
+    
+    public static void mostrarSumaCol(int[][] mat){
+        for (int col = 0; col < mat[0].length; col++) {
+            int contador = 0;
+            for (int fila = 0; fila < mat.length; fila++) {
+                contador+= mat[fila][col];
+            }
+            System.out.println("Suma de col " + col + " = " + contador);
+        }
+    }
+
+    public static boolean tieneTresConsecutivosEnFila(int[][] mat){
+        for (int i = 0; i < mat.length; i++) {
+            int contador = 1;
+            for (int j = 0; j < mat[i].length - 1; j++) {
+                if(mat[i][j] == mat[i][j+1]){
+                    contador ++;
+                    if(contador == 3) return true;
+                }else{
+                    contador = 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int maximoEntreCol(int[][] mat, int fila1, int fila2){
+        int max = mat[fila1][0];
+        for (int col = 0; col < mat[fila1].length; col++) {
+            if(mat[fila1][col] > max){
+                max = mat[fila1][col];
+            }
+            if(mat[fila2][col] > max){
+                max = mat[fila2][col];
+            }
+        }
+        return max;
+    }
+    
+    public static void diagonalInversa(int[][] mat){
+        for (int i = mat.length - 1; i >= 0; i--) {
+            System.out.println(mat[i][i]);
+        }
+    }
+    
+    public static void diagonalInversaRec(int[][] mat, int pos){
+        if(pos == 0) {
+            System.out.println(mat[pos][pos]);
+        }else{
+            System.out.println(mat[pos][pos]);
+            diagonalInversaRec(mat, pos - 1);
+        }
+    }
 }
